638f94c4eefa80999a10372843d87f2e1f59f4a6
David Copano <davidcopano96@gmail.com>
2021-05-22 14:30:14 +0200

    webtorrent

--- webtorrent/index.js
+++ webtorrent/index.js
@@ -1,17 +1,18 @@
-var WebTorrent = require('webtorrent');
+const WebTorrent = require('webtorrent');
+const chalk = require('chalk');
 
-var client = new WebTorrent();
-var torrentUri = 'https://webtorrent.io/torrents/big-buck-bunny.torrent';
+const client = new WebTorrent();
+const torrentUri = 'https://webtorrent.io/torrents/big-buck-bunny.torrent';
 
 client.add(torrentUri, function (torrent) {
-  console.log(`Descargando el torrent '${torrent.name}'...`)
+  console.log(chalk.blue(`Descargando el torrent '${torrent.name}'...`));
 
   torrent.on('download', (bytes) => {
-    console.log(`Progreso: ${(torrent.progress * 100).toFixed(2)}`);
+    const progressPercentage = (torrent.progress * 100).toFixed(2);
+    console.log(chalk.yellow(`Progreso: ${progressPercentage}%`));
 
-    if(torrent.progress === 1) {
-      console.log('Terminado!');
-      console.log(torrent.path);
+    if(progressPercentage >= 100) {
+      console.log(chalk.green(`Terminado! Se ha descargado en la siguiente carpeta: ${torrent.path}`));
     }
   })
 });

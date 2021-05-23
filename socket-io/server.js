cbc9d9d979a4813c59d75b2bc955b42471c1f44f
David Copano <davidcopano96@gmail.com>
2021-05-23 15:15:40 +0200

    socket io

---
+++ socket-io/server.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+  // send the index.html file for all requests
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3001, function () {
+  console.log('listening on *:3001');
+});
+
+//for testing, we're just going to send data to the client every second
+setInterval(function () {
+  /*
+    our message we want to send to the client: in this case it's just a random
+    number that we generate on the server
+  */
+  const msg = Math.random();
+  io.emit('message', msg);
+  console.log(msg);
+}, 1000);

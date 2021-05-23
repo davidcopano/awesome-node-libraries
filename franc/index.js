bd779611f2f68be92d497b670eb6c62f6d7bb806
David Copano <davidcopano96@gmail.com>
2021-05-23 14:35:49 +0200

    franc

---
+++ franc/index.js
@@ -0,0 +1,10 @@
+const franc = require('franc');
+
+console.log(franc('Alle menslike wesens word vry'));
+console.log(franc('এটি একটি ভাষা একক IBM স্ক্রিপ্ট'));
+console.log(franc('Alle menneske er fødde til fridom'));
+
+console.log(franc(''));
+
+console.log(franc('the'));
+console.log(franc('the', { minLength: 3 }));
